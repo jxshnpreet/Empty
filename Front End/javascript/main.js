017f3631c802da3ecd416feab9284e050892bd1d
jxshnpreet <2002jashanpreet@gmail.com>
2023-07-21 15:45:52 +0530

    Arrays in JS

--- Front End/javascript/main.js
+++ Front End/javascript/main.js
@@ -51,7 +51,69 @@ else{
 demo = -10
 demo >= 0 ? console.log(`${demo} + is a +ve no. `) : console.log(`${demo} + is -ve no. `)
 
-// Strings can be written in 3 ways
-// 1. ''
-// 2. ""
-// 3. ``
+// Strings can be written in 3 ways :- 1. '' 2. "" 3. `` 
+
+// Keywords in JS :- 
+// 1. let :- privste keyword access within a defined scope
+// 2. var :- public once defined can be access anywhere
+// 3. const :- it should be declared as well as defined , and value can't be changed once defined
+flag = true
+if (flag) {
+    let a = 10;
+    console.log(`The value of a inside if-block is ${a}`);
+}
+else{
+    b = 5
+}
+
+// Loops
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);  
+}
+
+let i = 0
+while (i<5) {
+    console.log(i);
+    i += 1
+}
+
+// Non-Primitive Data Types (Non-PDTs)
+// a.) Arrays
+let numbers = [10, 20, 30, 40, 50]
+let arr = []
+let arrObj = new Array(10)
+
+console.log(numbers)
+console.log(arr)
+console.log(arrObj)
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(numbers[i]);
+// }
+
+// Array Methods 
+numbers.push(60)
+console.log(numbers);
+
+let myArr1 = [100, 200, 300, 400, 500]
+// let myArr2 = myArr1  This is not a copy Mech.
+// Arrat Spread Operator
+let myArr2 = [ ...myArr1 ]
+myArr1.push(-10)
+myArr2.push(1000)
+console.log(myArr1);
+console.log(myArr2);
+
+myArr1.pop()
+console.log(myArr1);
+
+let deletedElement = myArr1.shift()
+console.log(myArr1);
+
+let newLength = myArr1.unshift(50)
+console.log(newLength);
+console.log(myArr1);
+
+let num1 = myArr1.concat(myArr2)
+console.log(num1);
